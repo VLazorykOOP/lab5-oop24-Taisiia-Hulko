d028931929f9f77dfb6b54378772682b86d876f7
unknown <hulko.taisiia@chnu.edu.ua>
2024-05-28 19:39:52 +0300

    12

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,190 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 
+using namespace std;
 
-#include <iostream>
+// базовий клас "Транспорт"
+class Transport {
+public:
+    virtual void print() = 0;
+    virtual ~Transport() {}
+};
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+// похідний клас "Автомобіль"
+class Car : public Transport {
+private:
+    string model; // модель автомобіля
+public:
+    Car(string m) : model(m) {}
+    void print() override {
+        cout << "This is a car, model: " << model << endl;
+    }
+    ~Car() {
+        cout << "Car destructor" << endl;
+    }
+};
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+// похідний клас "Вантажівка"
+class Truck : public Transport {
+private:
+    string model; // модель вантажівки
+public:
+    Truck(string m) : model(m) {}
+    void print() override {
+        cout << "This is a truck, model: " << model << endl;
+    }
+    ~Truck() {
+        cout << "Truck destructor" << endl;
+    }
+};
 
+// похідний клас "Корабель"
+class Ship : public Transport {
+private:
+    string name; // назва корабля
+public:
+    Ship(string n) : name(n) {}
+    void print() override {
+        cout << "This is a ship, name: " << name << endl;
+    }
+    ~Ship() {
+        cout << "Ship destructor" << endl;
+    }
+};
+
+// похідний клас "Літак"
+class Plane : public Transport {
+private:
+    string model; // модель літака
+public:
+    Plane(string m) : model(m) {}
+    void print() override {
+        cout << "This is a plane, model: " << model << endl;
+    }
+    ~Plane() {
+        cout << "Plane destructor" << endl;
+    }
+};
+
+// клас карти
+class Card {
+private:
+    string suit; // масть
+    string rank; // ранг
+public:
+    Card(string s, string r) : suit(s), rank(r) {}
+    void print() const {
+        cout << rank << " of " << suit << endl;
+    }
+    string getRank() const {
+        return rank;
+    }
+    string getSuit() const {
+        return suit;
+    }
+};
+
+// клас колоди карт
+class Deck {
+protected:
+    vector<Card> cards; // вектор карт
+public:
+    Deck() {}
+    Deck(bool shuffle) {
+        string suits[] = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        string ranks[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+        for (const string& suit : suits) {
+            for (const string& rank : ranks) {
+                cards.push_back(Card(suit, rank));
+            }
+        }
+        if (shuffle) {
+            shuffleDeck();
+        }
+    }
+    void shuffleDeck() {
+        srand(time(0));
+        random_shuffle(cards.begin(), cards.end());
+    }
+    void printDeck() const {
+        for (const Card& card : cards) {
+            card.print();
+        }
+    }
+    int getSize() const {
+        return cards.size();
+    }
+};
+
+// клас пасьянсу, що успадковує Deck
+class Solitaire : public Deck {
+public:
+    Solitaire(bool shuffle) : Deck(shuffle) {}
+    void removeSameColor() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < cards.size() - 2; ++j) {
+                if (cards[j].getSuit() == cards[j + 2].getSuit()) {
+                    if ((cards[j].getSuit() == "Hearts" || cards[j].getSuit() == "Diamonds") &&
+                        (cards[j + 2].getSuit() == "Hearts" || cards[j + 2].getSuit() == "Diamonds")) {
+                        cards.erase(cards.begin() + j, cards.begin() + j + 3);
+                    }
+                    else if ((cards[j].getSuit() == "Clubs" || cards[j].getSuit() == "Spades") &&
+                        (cards[j + 2].getSuit() == "Clubs" || cards[j + 2].getSuit() == "Spades")) {
+                        cards.erase(cards.begin() + j, cards.begin() + j + 3);
+                    }
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    int choice;
+
+    cout << "Введіть 1 для запуску коду Транспорту або 2 для запуску коду Карт: ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1: {
+        Car car("Toyota");
+        Truck truck("Ford");
+        Ship ship("Titanic");
+        Plane plane("Boeing 747");
+
+        car.print();
+        truck.print();
+        ship.print();
+        plane.print();
+        break;
+    }
+    case 2: {
+        Deck orderedDeck(false);
+        cout << "Ordered deck:" << endl;
+        orderedDeck.printDeck();
+        cout << "Size: " << orderedDeck.getSize() << endl << endl;
+
+        Deck shuffledDeck(true);
+        cout << "Shuffled deck:" << endl;
+        shuffledDeck.printDeck();
+        cout << "Size: " << shuffledDeck.getSize() << endl << endl;
+
+        Solitaire solitaireDeck(true);
+        cout << "Solitaire deck:" << endl;
+        solitaireDeck.removeSameColor();
+        solitaireDeck.printDeck();
+        cout << "Size: " << solitaireDeck.getSize() << endl;
+        break;
+    }
+    default: {
+        cout << "Невірний вибір" << endl;
+        break;
+    }
+    }
+
+    return 0;
 }
